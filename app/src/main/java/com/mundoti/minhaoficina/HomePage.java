aeba628e38f31f0e3de12e08175f0a8653b00a3c
Samuel <samuelprado9@hotmail.com>
2024-11-18 20:52:48 -0300

    Normalizando finais de linha

--- app/src/main/java/com/mundoti/minhaoficina/HomePage.java
+++ app/src/main/java/com/mundoti/minhaoficina/HomePage.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.Firebase;
 
 public class HomePage extends AppCompatActivity {
 
